a347b3974859612cd68a27d86bfd3dd4fd119546
mavioo7 <mavioo7@gmail.com>
2023-01-20 12:47:07 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is Dotnet Console App!");
+var sam=1;
+while (sam <= 5)
+{
+Console.WriteLine("This is Dotnet Console App!"+ " Iteration: "+ sam.ToString());
+sam++;
+}
+
+
